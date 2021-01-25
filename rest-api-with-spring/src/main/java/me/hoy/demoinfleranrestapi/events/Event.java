bc917e39b06cbe8ddf4cfa7199d97812e487c883
hoy172 <js.hoy172@gmail.com>
2021-01-25 22:49:39 +0900

    비즈니스 로직 적용

--- rest-api-with-spring/src/main/java/me/hoy/demoinfleranrestapi/events/Event.java
+++ rest-api-with-spring/src/main/java/me/hoy/demoinfleranrestapi/events/Event.java
@@ -28,5 +28,21 @@ public class Event {
     @Enumerated(EnumType.STRING)
     private EventStatus eventStatus = EventStatus.DRAFT;
 
+    public void update() {
+        //update free
+        if(this.basePrice == 0 && this.maxPrice ==0){
+            this.free = true;
+        }else{
+            this.free = false;
+        }
+        //update
+        if(this.location == null ||this.location.isBlank()){
+            this.offline = false;
+        }else{
+            this.offline = true;
+        }
+
+
+    }
 }
 //JAVA BEAN SPEC 보기
